c4d7158fa1cad66d5bc90bb98e5a401596e17459
18801089593 <18801089593@163.com>
2019-06-24 22:41:57 +0800

    6.24

--- practice/Day624.py
+++ practice/Day624.py
@@ -4,7 +4,12 @@ def sk(a_list,target):
         for j in range(i+1,n):
             if a_list[i] + a_list[j] == target:
                 print(i,j)
+                break
+        break
     else:
-        print("没有相关数据")
-a_list = [1,7,11,15,34,98,900]
+         print("没有相关数据")
+
+a_list = [1,7,11,15,34,98,100,900]
 sk(a_list,9)
+
+
