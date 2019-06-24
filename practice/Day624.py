0b2c5d15741aac01a2b32cf7949dc481e5263887
18801089593 <18801089593@163.com>
2019-06-24 22:18:35 +0800

    6.24

---
+++ practice/Day624.py
@@ -0,0 +1,10 @@
+def sk(a_list,target):
+    n = len(a_list)
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if a_list[i] + a_list[j] == target:
+                print(i,j)
+    else:
+        print("没有相关数据")
+a_list = [1,7,11,15,34,98,900]
+sk(a_list,9)
